0868bd8498e7e7b2f1a39ac26679c74b97fe599c
icemanlatimer76 <31551262+icemanlatimer76@users.noreply.github.com>
2019-06-08 11:07:29 +0200

    Add files via upload

--- Regle.java
+++ Regle.java
@@ -1,56 +1,52 @@
 import java.util.ArrayList;
-public class Regle
-{
-	//regle de base
-	private static final int PIECE_DEPART = 3 ;
-	private static final int NB_CARTE_ETEABLISSEMENT_BASE    = 6;
+
+public class Regle {
+	// regle de base
+	private static final int PIECE_DEPART = 3;
+	private static final int NB_CARTE_ETEABLISSEMENT_BASE = 6;
 	private static final int NB_CARTE_ETEABLISSEMENT_SPECIAL = 4;
-	//String de toute les cartes qu'on veux ajouter au jeu
-	private static final String[] etablisementBase= {"champs","ferme","boulangerie","cafe","superette","foret","fromagerie","fabriqueMeuble","mine","restaurant","verger","marcherLegume"};
-	private static final String[] etablisementSpecial={"stade","chaineTV","centreAffaire"};
+	// String de toute les cartes qu'on veux ajouter au jeu
+	private static final String[] etablisementBase = { "champs", "ferme", "boulangerie", "cafe", "superette", "foret",
+			"fromagerie", "fabriqueMeuble", "mine", "restaurant", "verger", "marcherLegume" };
+	private static final String[] etablisementSpecial = { "stade", "chaineTV", "centreAffaire" };
 
-	public static void initialisation(ArrayList<Joueur> joueurs,Banque banque)
-	{
+	public static void initialisation(ArrayList<Joueur> joueurs, Banque banque) {
 
-		//creation de la banque
-		//etablissement de base
-		for(int i = 0; i<Regle.etablisementBase.length;i++)
-		{
-			Carte tmp =EnumCarte.valueOf(Regle.etablisementBase[i]).creeCarte();
-			Regle.creeCarte(banque,tmp,Regle.NB_CARTE_ETEABLISSEMENT_BASE);
+		// creation de la banque
+		// etablissement de base
+		for (int i = 0; i < Regle.etablisementBase.length; i++) {
+			Carte tmp = EnumCarte.valueOf(Regle.etablisementBase[i]).creeCarte();
+			Regle.creeCarte(banque, tmp, Regle.NB_CARTE_ETEABLISSEMENT_BASE);
 		}
-		//etablissement special
-		for(int i = 0; i<Regle.etablisementSpecial.length;i++)
-		{
-			Carte tmp =EnumCarte.valueOf(Regle.etablisementSpecial[i]).creeCarte();
-			Regle.creeCarte(banque,tmp,Regle.NB_CARTE_ETEABLISSEMENT_SPECIAL);
+		// etablissement special
+		for (int i = 0; i < Regle.etablisementSpecial.length; i++) {
+			Carte tmp = EnumCarte.valueOf(Regle.etablisementSpecial[i]).creeCarte();
+			Regle.creeCarte(banque, tmp, Regle.NB_CARTE_ETEABLISSEMENT_SPECIAL);
 		}
 
-		//cree les carte donner au joueur au debut
-		Carte champs      = EnumCarte.champs.creeCarte();
+		// cree les carte donner au joueur au debut
+		Carte champs = EnumCarte.champs.creeCarte();
 		Carte boulangerie = EnumCarte.boulangerie.creeCarte();
-		Carte tmp ;
-		for(Joueur joueur : joueurs)
-		{
+		Carte tmp;
+		for (Joueur joueur : joueurs) {
 			joueur.setPiece(Regle.PIECE_DEPART);// ajout du nombre de piece de depart
-			tmp=banque.retirer("champs de ble");
-			if(tmp != null )joueur.ajouterCarte(tmp);
-			tmp=banque.retirer("boulangerie");
-			if(tmp != null )joueur.ajouterCarte(tmp);
+			tmp = champs;
+			if (tmp != null)
+				joueur.ajouterCarte(tmp);
+			tmp = boulangerie;
+			if (tmp != null)
+				joueur.ajouterCarte(tmp);
 		}
 
+	}
+
+	public static void tourDeJeu() {
 
 	}
 
-	public static void tourDeJeu()
-	{
-		
-	}
-	//cree est ajoute un certain nombre de carte a la banque
-	private static void creeCarte(Banque banque ,Carte carte , int nb)
-	{
-		for(int i=0;i<nb;i++)
-		{
+	// cree est ajoute un certain nombre de carte a la banque
+	private static void creeCarte(Banque banque, Carte carte, int nb) {
+		for (int i = 0; i < nb; i++) {
 			banque.ajouter(new Carte(carte));
 		}
 	}
